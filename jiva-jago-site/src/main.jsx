77e1e6eb8be92ec576fdf71379c4a04ef47f8661
Kenneth Basjan <92210924+Abbbtle@users.noreply.github.com>
2025-05-30 16:19:12 +0200

    main.jsx updated

--- jiva-jago-site/src/main.jsx
+++ jiva-jago-site/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import App from './App.jsx';
+import About_Page from './pages/About_Page.jsx'
+import Contact_Page from './pages/Contact_Page.jsx'
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Define your routes here
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/categories",
+    element: <Categories />,
+    children: [
+      // Nested routes
+    ]
+  },
+  {
+    path: "/about",
+    element: <About_Page />,
+  },
+  {
+    path: "/contact",
+    element: <Contact_Page />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} /> {/* Pass router to RouterProvider */}
+  </React.StrictMode>,
+);
